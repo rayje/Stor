4da1e366b179899c836fe30b4d2226320cf6e213
Rayland <dev.rayje@gmail.com>
2013-10-27 11:37:58 -0600

    Update install scripts to install Stor in /usr/local

--- bin/env.sh
+++ bin/env.sh
@@ -3,9 +3,6 @@
 export MY_HOME=$(getent passwd $(whoami) | cut -d: -f6)
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 
-NAME=StorInstall
-STOR_REPO=https://github.com/rayje/Stor.git
-PASTRY_URL=http://www.freepastry.org/FreePastry
 PASTRY_TAR=FreePastry-2.1-source.tgz
+PASTRY_URL=http://www.freepastry.org/FreePastry/$PASTRY_TAR
 LOCAL_PASTRY=/tmp/pastry
-export MY_HOME=$(getent passwd $(whoami) | cut -d: -f6)
--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+#==========================================================
+# The install script will install all the dependencies
+# required by Stor. Installation involves the following
+# steps:
+#   1. Download Stor source code.
+#   2. Download FreePastry
+#   3. Download and install UpnpLib into local Maven repo
+#   4. Compile and Install FreePastry to local Maven repo
+#   5. Compile Stor
+#   6. Install Stor at /usr/local/stor
+#==========================================================
 
 
 if [ "$(id -u)" != "0" ]; then
@@ -6,22 +17,14 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-wget -q https://raw.github.com/rayje/Stor/master/tools/env.sh -P /tmp
-. /tmp/env.sh
-rm /tmp/env.sh
-
-if [ "$JAVA_HOME" = "" ]; then
-    echo "Could not find java. Aborting."
-    exit 1
-else 
-    echo "Found java at $JAVA_HOME"
-fi
-
 if ! type mvn > /dev/null; then
     echo "Maven is required but not installed. Aborting."
     exit 1
 fi
 
+STOR_INSTALL_DIR=${STOR_INSTALL_DIR:-/usr/local/Stor}
+STOR_REPO=${STOR_REPO:-https://github.com/rayje/Stor.git}
+NAME=StorInstall
 TMP=$(mktemp -d /tmp/stor.XXXXXXXXXX) || { echo "Failed to create temp dir"; exit 1; }
 export PASTRY_DIR=$TMP/pastry-2.1
 
@@ -47,7 +50,7 @@ install_pastry() {
     fi
 
     if [ ! -f $LOCAL_PASTRY/$PASTRY_TAR ]; then
-        wget "$PASTRY_URL/$PASTRY_TAR" -P $LOCAL_PASTRY
+        wget "$PASTRY_URL" -P $LOCAL_PASTRY
         if [ ! -f $LOCAL_PASTRY/$PASTRY_TAR ]; then
             log "Unable to get $PASTRY_URL/$PASTRY_TAR. Aborting"
             exit 1
@@ -86,12 +89,44 @@ compile_stor() {
     mvn compile
 }
 
+install_stor() {
+    if [ ! -d $STOR_INSTALL_DIR ]; then
+        mkdir -p $STOR_INSTALL_DIR
+    else
+        STOR_BAK=/tmp/stor.$(date +"%m_%d_%Y_%H_%M_%S").tar
+        tar -cf $STOR_BAK $STOR_INSTALL_DIR
+        log "Created backup of previous Stor install at $STOR_BAK"
+    fi
+ 
+    cp -R $TMP/Stor/bin $STOR_INSTALL_DIR
+    chmod 755 $STOR_INSTALL_DIR/bin/*
+
+    ln -s $STOR_INSTALL_DIR/bin/stor /usr/local/bin/stor
+}
+
 trap on_exit EXIT
 
 log "Created tmp: $TMP"
 cd $TMP
 
 clone_stor
+
+if [ ! -f $TMP/Stor/bin/env.sh ]; then
+    log "Unable to source Stor environment. Aborting"
+    exit 1
+fi
+
+. $TMP/Stor/bin/env.sh
+
+if [ "$JAVA_HOME" = "" ]; then
+    echo "Could not find java. Aborting."
+    exit 1
+else 
+    echo "Found java at $JAVA_HOME"
+fi
+
 install_pastry
 compile_stor
+install_stor
 
+log "Stor successfully installed"
