b9f06302bfc0ce1df594588cf2c31e4414ea9fe2
Rayland <dev.rayje@gmail.com>
2013-10-27 13:36:02 -0600

    Update stor script to start server and client

--- bin/env.sh
+++ bin/env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export MY_HOME=$(getent passwd $(whoami) | cut -d: -f6)
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
 PASTRY_TAR=FreePastry-2.1-source.tgz
 PASTRY_URL=http://www.freepastry.org/FreePastry/$PASTRY_TAR
