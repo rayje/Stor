155b611a2523c124638d56d7abb90c550b74cd00
Rayland <dev.rayje@gmail.com>
2013-10-27 00:27:26 -0600

    Update pastry setup

--- tools/pastry-setup.sh
+++ tools/pastry-setup.sh
@@ -5,9 +5,10 @@
 # in other Maven projects.
 #==============================================================================
 
+MY_HOME=${MY_HOME:-$HOME}
 STOR_TOOLS_DIR=$(pwd)
-M2_REPO=${M2_REPO:-~/.m2/repository}
-PASTRY_DIR=${PASTRY_DIR:-~/Documents/Java_Library/pastry-2.1}
+M2_REPO=${M2_REPO:-$MY_HOME/.m2/repository}
+PASTRY_DIR=${PASTRY_DIR:-/tmp/stor.9NpF8sSOrr/pastry-2.1}
 UPNPLIB_REPO=$M2_REPO/net/sbbi/sbbi-upnplib/1.0.4
 
 #==============================================
@@ -17,6 +18,7 @@ if [ ! -d $UPNPLIB_REPO ]; then
     mkdir -p $UPNPLIB_REPO
     cd $UPNPLIB_REPO
     wget http://repo.karatachi.org/mvn/net/sbbi/sbbi-upnplib/1.0.4/sbbi-upnplib-1.0.4.jar
+    echo "UpnpLib installed at $UPNPLIB_REPO"
 
     echo "\n==============================================================="
     if [ -f $UPNPLIB_REPO/sbbi-upnplib-1.0.4.jar ]; then
@@ -25,6 +27,10 @@ if [ ! -d $UPNPLIB_REPO ]; then
         echo "An problem occurred while trying to add UpnpLib to the Maven Repository"
         exit 1
     fi
+    echo "===============================================================\n"
+else
+    echo "\n==============================================================="
+    echo "UpnpLib is already installed at $UPNPLIB_REPO"
     echo "===============================================================\n"
 fi
 
@@ -41,7 +47,7 @@ if [ ! -d $M2_REPO/pastry ]; then
     cp pastry.pom $PASTRY_DIR/pom.xml
 
     cd $PASTRY_DIR
-    mvn install
+    mvn -X install
 
     echo "\n==============================================================="
     if [ -f $M2_REPO/pastry/pastry/2.1/pastry-2.1.jar ]; then
