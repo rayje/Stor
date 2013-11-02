c62009e7b64d55e021d9ed47ca4304e66b0d5bb4
ssharma <ssharma@colorado.edu>
2013-11-01 21:22:55 -0600

    When a client receives a valid response for the GET command, it now moves the file from the temp directory to the root of the working directory for the application.

--- src/main/java/com/stor/client/ClientHandler.java
+++ src/main/java/com/stor/client/ClientHandler.java
@@ -4,6 +4,10 @@ import com.stor.commands.*;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,7 +28,46 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
 
     public void channelRead(ChannelHandlerContext ctx, Object result) throws Exception {
         logger.log(Level.INFO, "" + result);
+
+        if (result instanceof CommandResult) {
+            CommandResult commandResult = (CommandResult) result;
+            switch (command.getType()) {
+                case GET:
+                    handleGetResponse(commandResult);
+                    break;
+                case PUT:
+                    handlePutResponse(commandResult);
+                    break;
+                default:
+                    logger.warning("Unexpected Command: " + command);
+            }
+        } else {
+            logger.warning("Unknown response for a command from the server");
+        }
+
         ctx.close();
+    }
+
+    private void handlePutResponse(CommandResult commandResult) {
+        logger.info("handlePutResponse is not implemented yet. Result: " + commandResult);
+    }
+
+    private void handleGetResponse(CommandResult commandResult) throws IOException {
+        logger.info("handleGetResponse");
+
+        if (commandResult.getResultType() == ResultType.FAILURE) {
+            logger.info("Failed to complete the get command: " + commandResult.getErrorMessage());
+        } else {
+            final Path serverFilePath = Paths.get((String) commandResult.getResult());
+            logger.info("File saved to: " + serverFilePath);
+
+            if (!serverFilePath.isAbsolute()) {
+                logger.severe("Server must return absolute file path");
+            } else {
+                Files.move(serverFilePath, serverFilePath.getFileName());
+                logger.info("File moved successfully: From: " + serverFilePath + "To: " + serverFilePath.getFileName().toAbsolutePath());
+            }
+        }
     }
 
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
