cd936b1037e6c534ff589ebc9ea2192ee187f0b3
Rayland <dev.rayje@gmail.com>
2013-11-23 17:49:47 -0700

    Fix bugs in scripts

--- tools/util.sh
+++ tools/util.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 get_ips() {
-	#euca-describe-instances | grep ami-00000200 | awk '{print $4}'
-	cat ips > /tmp/ips
-	echo "/tmp/ips"
+	if [ ! -d ".tmp" ]; then
+		mkdir -p .tmp
+	fi
+
+	euca-describe-instances | grep ami-00000200 | awk '{print $4}' > .tmp/ips
+	echo >> .tmp/ips
+	echo ".tmp/ips"
+}
+
+list_inst() {
+	euca-describe-instances | grep ami-00000200
 }
