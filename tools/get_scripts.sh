c4c73a8fa18a4dc352e33b9b005ee0d49f78741f
Rayland <dev.rayje@gmail.com>
2013-11-23 18:51:26 -0700

    Add more scripts

--- tools/get_scripts.sh
+++ tools/get_scripts.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-scripts="start-all util.sh"
+scripts="start-all util.sh list-storage ub"
+
+if [ ! -d $HOME/bin ];
+	mkdir $HOME/bin
+fi
 
 for script in $scripts; do 
-	wget "https://raw.github.com/rayje/Stor/master/tools/$script" .
+	wget "https://raw.github.com/rayje/Stor/master/tools/$script" -P $HOME/bin/
+	chmod 755 $HOME/bin/$script
 done
